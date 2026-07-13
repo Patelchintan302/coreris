7b0e16ed92d6a60b079dbaa61b539ff3da9e3f61
Patelchintan302 <patelcg302@gmail.com>
2026-07-13 18:23:12 +0530

    working on DTOs

--- coreris/src/main/java/com/example/coreris/controller/AppointmentController.java
+++ coreris/src/main/java/com/example/coreris/controller/AppointmentController.java
@@ -12,7 +12,7 @@ import java.net.URI;
 import java.util.List;
 
 @RequiredArgsConstructor
-@RequestMapping("/appointment")
+@RequestMapping("/appointments")
 @RestController
 public class AppointmentController {
     private final AppointmentService appointmentService;
