f17e7ff2881df530da045950d4e207019e0a9d9f
Patelchintan302 <patelcg302@gmail.com>
2026-07-22 12:42:27 +0530

    fixing workflow

--- coreris/src/main/java/com/example/coreris/dto/ScanResultDto.java
+++ coreris/src/main/java/com/example/coreris/dto/ScanResultDto.java
@@ -1,7 +1,6 @@
 package com.example.coreris.dto;
 
 import lombok.*;
-
 import java.time.LocalDateTime;
 
 @Data
--- coreris/src/main/java/com/example/coreris/repository/ScanResultRepository.java
+++ coreris/src/main/java/com/example/coreris/repository/ScanResultRepository.java
@@ -1,11 +1,12 @@
 package com.example.coreris.repository;
 
-import com.example.coreris.entity.Report;
 import com.example.coreris.entity.ScanResult;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface ScanResultRepository extends JpaRepository<ScanResult, Long> {
     Optional<ScanResult> findByAppointmentId(Long appointmentId);
 }
