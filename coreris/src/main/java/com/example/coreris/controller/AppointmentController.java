58deaa20a76c2ec86b3dbe62d060663986333828
Patelchintan302 <patelcg302@gmail.com>
2026-07-30 18:36:13 +0530

    implementing soft delete for appointment and trying to implement spring security

--- coreris/src/main/java/com/example/coreris/controller/AppointmentController.java
+++ coreris/src/main/java/com/example/coreris/controller/AppointmentController.java
@@ -15,7 +15,6 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
-import java.util.List;
 
 @RequiredArgsConstructor
 @RequestMapping("/appointments")
